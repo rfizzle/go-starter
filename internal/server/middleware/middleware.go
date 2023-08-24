e6fa738f4a94f9f36c5cdd4a577de2e4b85e3eeb
Coleton Pierson <coleton.pierson@gmail.com>
2023-08-24 14:42:45 +0700

    feat: added swagger and redoc UI into middleware

--- internal/server/middleware/middleware.go
+++ internal/server/middleware/middleware.go
@@ -1,21 +1,23 @@
 package middleware
 
 import (
-	"net/http"
-	"time"
+  "net/http"
+  "time"
 
-	"go.uber.org/zap"
+  "go.uber.org/zap"
 
-	"github.com/go-chi/chi/v5/middleware"
+  "github.com/go-chi/chi/v5/middleware"
 )
 
 func New(logger *zap.Logger, apiHandler, staticHandler http.Handler) http.Handler {
-	return NewChain(
-		middleware.Timeout(60*time.Second),
-		middleware.RequestID,
-		middleware.RealIP,
-		zapLogger(logger),
-		middleware.Recoverer,
-		fileServer(staticHandler),
-	).Then(apiHandler)
+  return NewChain(
+    middleware.Timeout(60*time.Second),
+    middleware.RequestID,
+    middleware.RealIP,
+    zapLogger(logger),
+    middleware.Recoverer,
+    SwaggerUI(),
+    RedocUI(),
+    fileServer(staticHandler),
+  ).Then(apiHandler)
 }
---
+++ internal/server/middleware/swagger.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+  "net/http"
+
+  "github.com/go-openapi/runtime/middleware"
+)
+
+func SwaggerUI() func(next http.Handler) http.Handler {
+  return func(next http.Handler) http.Handler {
+    opts := middleware.SwaggerUIOpts{}
+    opts.EnsureDefaults()
+    opts.BasePath = "/api/docs"
+    opts.Path = "swagger"
+    return middleware.SwaggerUI(opts, next)
+  }
+}
+
+func RedocUI() func(next http.Handler) http.Handler {
+  return func(next http.Handler) http.Handler {
+    opts := middleware.RedocOpts{}
+    opts.EnsureDefaults()
+    opts.BasePath = "/api/docs"
+    opts.Path = "redoc"
+    return middleware.Redoc(opts, next)
+  }
+}
